1311a7b20aea568ee1ec26146f7c066b93ce2071
dranreb22 <bernard2200@hotmail.com>
2019-12-02 20:10:49 -0500

    fixed conflicts

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -8,17 +8,17 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("HomePage.fxml"));
-        primaryStage.setTitle("Animal Shelter Manager");
-        primaryStage.setScene(new Scene(root, 600, 400));
-        primaryStage.show();
+  @Override
+  public void start(Stage primaryStage) throws Exception {
+    Parent root = FXMLLoader.load(getClass().getResource("HomePage.fxml"));
+    primaryStage.setTitle("Animal Shelter Manager");
+    primaryStage.setScene(new Scene(root, 600, 400));
+    primaryStage.show();
 
-    }
+  }
 
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+  public static void main(String[] args) {
+    launch(args);
+  }
 }
