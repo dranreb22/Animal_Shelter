060dd59135fb516470a218e0b0573aca443d3dac
Bernard Georges <bernard2200@hotmail.com>
2019-12-03 15:39:22 -0500

    Update AnimalSearchController.java

--- src/sample/AnimalSearchController.java
+++ src/sample/AnimalSearchController.java
@@ -7,12 +7,48 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.layout.AnchorPane;
 
 public class AnimalSearchController {
+
   @FXML
   private AnchorPane rootPane;
 
   @FXML
+  public void handleAdoptableDogsMenuItem(ActionEvent actionEvent) throws IOException {
+    AnchorPane pane = FXMLLoader.load(getClass().getResource("AdoptableDogs.fxml"));
+    rootPane.getChildren().setAll(pane);
+  }
+
+  @FXML
+  public void handleHomeMenuItem(ActionEvent actionEvent) throws IOException {
+    AnchorPane pane = FXMLLoader.load(getClass().getResource("HomePage.fxml"));
+    rootPane.getChildren().setAll(pane);
+  }
+  @FXML
+  public void handleAdoptableCatsMenuItem(ActionEvent actionEvent) throws IOException {
+    AnchorPane pane = FXMLLoader.load(getClass().getResource("AdoptableCats.fxml"));
+    rootPane.getChildren().setAll(pane);
+  }
+
+  @FXML
+  public void handleAnimalInfoMenuItem(ActionEvent actionEvent) throws IOException {
+    AnchorPane pane = FXMLLoader.load(getClass().getResource("AnimalInformation.fxml"));
+    rootPane.getChildren().setAll(pane);
+  }
+
+  @FXML
   public void handleAnimalCareMenuItem(ActionEvent actionEvent) throws IOException {
     AnchorPane pane = FXMLLoader.load(getClass().getResource("AnimalCare.fxml"));
+    rootPane.getChildren().setAll(pane);
+    System.out.println("You would like to adopt a dog!!");
+  }
+
+  @FXML
+  public void handleAnimalSearchMenuItem(ActionEvent actionEvent) throws IOException {
+    AnchorPane pane = FXMLLoader.load(getClass().getResource("AnimalSearch.fxml"));
+    rootPane.getChildren().setAll(pane);
+  }
+  @FXML
+  public void handleAnimalStatusMenuItem(ActionEvent actionEvent) throws IOException {
+    AnchorPane pane = FXMLLoader.load(getClass().getResource("AnimalStatus.fxml"));
     rootPane.getChildren().setAll(pane);
   }
 
